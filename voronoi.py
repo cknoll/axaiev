0a377a7d64013c9d17b0819ed277f9d420e55bb8
Carsten Knoll <carsten.knoll@posteo.de>
2024-11-29 21:32:12 +0100

    voronoi.py: oo refactoring

--- voronoi.py
+++ voronoi.py
@@ -10,27 +10,12 @@ from ipydex import IPS, activate_ips_on_exception
 activate_ips_on_exception()
 
 
-def create_mesh(polygon: Polygon, num_points):
-    minx, miny, maxx, maxy = polygon.bounds
-    x_coords = np.linspace(minx, maxx, int(np.sqrt(num_points)) + 1)
-    y_coords = np.linspace(miny, maxy, int(np.sqrt(num_points)) + 1)
-
-    mesh_points = []
-    # note: by `[1:-1]` we reduce the chance of possible boundary points
-    for x in x_coords[1:-1]:
-        for y in y_coords[1:-1]:
-            point = (x, y)
-            if polygon.contains(Point(point)):
-                mesh_points.append(point)
-
-    return np.array(mesh_points)
-
-
 def plot_polygons(polygons: list[Polygon], **kwargs):
     for pg in polygons:
         if isinstance(pg, MultiPolygon):
             plot_polygons(pg.geoms)
         plot_polygon_like_obj(pg, **kwargs)
+
 
 def plot_polygon_like_obj(obj: Polygon, **kwargs):
 
@@ -45,63 +30,88 @@ def plot_polygon_like_obj(obj: Polygon, **kwargs):
 
     plt.fill(x_poly, y_poly, alpha=0.5, **kwargs_used)
 
-# Example usage
-# Create an arbitrary polygon (non-convex in this case)
-exterior = [(0, 0), (4, 0), (4, 4), (2, 3), (0, 4)]
-hole = [] # [(1, 1), (2, 1), (2, 2), (1, 2)]
-main_pg = Polygon(exterior, [hole])
 
-bound = main_pg.buffer(2).envelope.boundary #Create a large rectangle surrounding it
+class VoronoiMesher:
+    def __init__(self, main_pg: Polygon):
+        self.main_pg = main_pg
+        self.inner_polys = None
+        self.boundary_coords = None
+        self.inner_points = None
+        self.vor = None
+        self.voronoi_polys = None
+        self._ip_xx = None
+        self._ip_yy = None
+        self.xmin, self.ymin, self.xmax, self.ymax = self.main_pg.bounds
+
+    def create_voronoi_mesh_for_polygon(self, num_points=100) -> list[Polygon]:
+
+        self.inner_points = self.create_mesh_for_inner_points(num_points)
+        # diff_x = self.xmax - self.xmin
+        # diff_y = self.ymax - self.ymin
+
+        diff_x = np.diff(self._ip_xx[:2])[0]
+        diff_y = np.diff(self._ip_yy[:2])[0]
+
+        dist = np.max((diff_x, diff_y))*2  # double the inner mesh-parameter to "ensure" the cell reaches outward
+        boundary_pg = self.main_pg.buffer(dist).envelope.boundary #Create a large rectangle surrounding it
+
+        boundary_points = [boundary_pg.interpolate(distance=d, normalized=True) for d in np.linspace(0, 1, 100)[:-1]]
+        self.boundary_coords = np.array([[p.x, p.y] for p in boundary_points])
+
+        # Create an array of all points on the boundary and inside the polygon
+        self.all_coords = np.concatenate((self.boundary_coords, self.inner_points))
+
+        self.vor = Voronoi(points=self.all_coords)
+        lines = [LineString(self.vor.vertices[line]) for line in self.vor.ridge_vertices if -1 not in line]
+
+        self.voronoi_polys = list(polygonize(lines))
+
+        pg: Polygon
+        self.inner_polys: list[Polygon] = []
+        for pg in self.voronoi_polys:
+            intersection_pg: Polygon = pg.intersection(self.main_pg)
+            if intersection_pg.is_empty:
+                continue
+            self.inner_polys.append(intersection_pg)
+
+        return self.inner_polys
+
+    def create_mesh_for_inner_points(self, num_points):
+        minx, miny, maxx, maxy = self.main_pg.bounds
+
+        # note: by `[1:-1]` we reduce the chance of possible boundary points
+        self._ip_xx = np.linspace(minx, maxx, int(np.sqrt(num_points)) + 1)[1:-1]
+        self._ip_yy = np.linspace(miny, maxy, int(np.sqrt(num_points)) + 1)[1:-1]
+
+        mesh_points = []
+        for x in self._ip_xx:
+            for y in self._ip_yy:
+                point = (x, y)
+                if self.main_pg.contains(Point(point)):
+                    mesh_points.append(point)
+
+        return np.array(mesh_points)
 
 
-boundary_points = [bound.interpolate(distance=d, normalized=True) for d in np.linspace(0, 1, 100)[:-1]]
-boundary_coords = np.array([[p.x, p.y] for p in boundary_points])
+if __name__ == "__main__":
 
-inner_points = create_mesh(main_pg, 100)
+    # Example usage
+    # Create an arbitrary polygon (non-convex in this case)
+    exterior = [(0, 0), (4, 0), (4, 4), (2, 3), (0, 4)]
+    hole = [] # [(1, 1), (2, 1), (2, 2), (1, 2)]
+    main_pg = Polygon(exterior, [hole])
 
-# all_coords = inner_points
+    vm = VoronoiMesher(main_pg)
+    inner_polys = vm.create_voronoi_mesh_for_polygon()
 
-# Create an array of all points on the boundary and inside the polygon
-all_coords = np.concatenate((boundary_coords, inner_points))
+    # plot_polygons(lines, ec="tab:green")
+    plot_polygons(inner_polys, ec="tab:green")
+    # IPS()
 
-vor = Voronoi(points=all_coords)
-lines = [LineString(vor.vertices[line]) for line in vor.ridge_vertices if -1 not in line]
+    plot_polygon_like_obj(main_pg)
+    # plot_polygon_like_obj(bound)
 
-polys = list(polygonize(lines))
+    plt.scatter(*vm.inner_points.T, color='magenta', s=10, alpha=0.5)
+    plt.scatter(*vm.boundary_coords.T, color='tab:orange', s=10, alpha=0.5)
 
-pg: Polygon
-inner_polys = []
-for pg in polys:
-    intersection_pg: Polygon = pg.intersection(main_pg)
-    if intersection_pg.is_empty:
-        continue
-    inner_polys.append(intersection_pg)
-
-IPS()
-
-# plot_polygons(lines, ec="tab:green")
-plot_polygons(inner_polys, ec="tab:green")
-# IPS()
-
-plot_polygon_like_obj(main_pg)
-# plot_polygon_like_obj(bound)
-
-plt.scatter(*inner_points.T, color='magenta', s=10, alpha=0.5)
-plt.scatter(*boundary_coords.T, color='tab:orange', s=10, alpha=0.5)
-
-plt.show()
-exit()
-# Generate Voronoi mesh
-N = 100  # Number of inner points
-voronoi_cells = generate_voronoi_mesh(main_pg, N)
-
-IPS()
-
-# Visualization
-fig, ax = plt.subplots(figsize=(10, 10))
-plot_polygon_like_obj(main_pg, ec='red', fc='none', lw=2)
-plot_polygons(voronoi_cells)
-# GeoSeries(polygon).plot(ax=ax, edgecolor='red', facecolor='none', linewidth=2)
-ax.set_title(f'Voronoi-like Mesh with {N} Inner Points')
-plt.axis('equal')
-plt.show()
+    plt.show()
